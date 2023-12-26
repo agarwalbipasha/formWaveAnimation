eafe4ff17010ee125589728bef4b582f7aff7be9
Bipasha Agarwal <62586833+agarwalbipasha@users.noreply.github.com>
2023-12-26 14:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ labels.forEach((label) => {
     .split("")
     .map(
       (letter, index) =>
-        `<span style="transition-delay:${index * 50}ms`">${letter}</span>`
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
     )
     .join("");
 });
